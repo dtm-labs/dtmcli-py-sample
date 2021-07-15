31b03a109b4021424e6324d31cbf7ddbaf5c68c5
yedongfu <dongfuye@163.com>
2021-07-15 13:59:48 +0800

    update sample

--- app.py
+++ app.py
@@ -27,15 +27,21 @@ def trans_in_confirm():
 def trans_in_cancel():
     return {"result": "SUCCESS"}
 
+# 这是dtm服务地址
 dtm = "http://localhost:8080/api/dtmsvr"
+# 这是业务微服务地址
 svc = "http://localhost:5000/api"
 
 @app.get("/api/fireTcc")
 def fire_tcc():
+    # 发起tcc事务
     gid = tcc.tcc_global_transaction(dtm, tcc_trans)
     return {"gid": gid}
 
+# tcc事务的具体处理
 def tcc_trans(t):
-    req = {"amount": 30}
+    req = {"amount": 30} # 业务请求的负荷
+    # 调用转出服务的Try|Confirm|Cancel
     t.call_branch(req, svc + "/TransOutTry", svc + "/TransOutConfirm", svc + "/TransOutCancel")
+    # 调用转入服务的Try|Confirm|Cancel
     t.call_branch(req, svc + "/TransInTry", svc + "/TransInConfirm", svc + "/TransInCancel")
